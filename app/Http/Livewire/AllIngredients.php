a52f76805b7c27af9c1940fea23ebfe5ac8c5475
casanoir <mendoun@hotmail.de>
2022-05-24 23:46:18 +0200

    show all ingredients & add listeners to Ingre info

--- app/Http/Livewire/AllIngredients.php
+++ app/Http/Livewire/AllIngredients.php
@@ -11,38 +11,31 @@ class AllIngredients extends Component
     
     public $alphabets=['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'];
     public $ingredientsAlphabetically;
-    public $ingredient;
     
-    protected $listeners =['ingredientInfo'=>'$refresh'];
+    protected $listeners =['ingredientInfo'=>'getIngerdients'];
 
-    public function render()
-    {
-        
+    public function render(){
         return view('livewire.all-ingredients');
     }
+    public function mount(){
+        $this->getIngerdients();
+    }
    
-    public function mount()
-    {
+    // 
+    public function getIngerdients(){
         $this->ingredientsAlphabetically = DB::table('ingredients')->get();
     }
 
-    
-    public function getIngerdientsAlphabetically($alphabet)
-    {
+    // 
+    public function getIngerdientsAlphabetically($alphabet){
         $this->ingredientsAlphabetically = DB::table('ingredients')->where('name','like',$alphabet.'%')->orderBy('name')->get();
-        
     }
+
+    // 
     public function ingredientInfo($apiIngredientId){
-
         $this->apiIngredientId=$apiIngredientId;
-        dd($this->apiIngredientId);
-        $this->emit('ingredientInfo');
-        
-        // $this->ingredient=$ingredient;
-        // $this->ingredientName=$ingredientName;
-        // $this->ingredientsAlphabetically = DB::table('ingredients')->where('name','like',$ingredient.'%')->orderBy('name')->get();
-        // $this->emit('ingredientInfo',$this->apiIngredientId);
-
+        $this->emit('ingredientInfo',$apiIngredientId);
+        $this->getIngerdients();
     }
     
 }
--- resources/views/livewire/all-ingredients.blade.php
+++ resources/views/livewire/all-ingredients.blade.php
@@ -1,19 +1,15 @@
-           
-            <div >
-                {{-- show all alphabets --}}
-                @foreach ($alphabets as $alphabet)
-                    <button wire:click="getIngerdientsAlphabetically($event.target.innerText)">{{$alphabet}}</button>
-                @endforeach
+<div >
+    {{-- show all alphabets --}}
+    @foreach ($alphabets as $alphabet)
+        <button wire:click="getIngerdientsAlphabetically($event.target.innerText)">{{$alphabet}}</button>
+    @endforeach
 
-                {{-- Show all ingredients alphabetically --}}
-                @foreach ($ingredientsAlphabetically as $ingredient)
-                    {{-- Ingredient Name --}}
-                    <p>{{$ingredient->name}} </p>    
-                    {{-- submit-form to show ingredient details from the api with parameter: apiIngredientId --}}
-                    {{-- <form action="#" wire:submit.prevent="ingredientInfo({{$ingredient->apiIngredientId}})">
-                        <button >show info</button>
-                    </form> --}}
-                    <button wire:click="ingredientInfo({{$ingredient->apiIngredientId}})">show info</button>
+    {{-- Show all ingredients alphabetically --}}
+    @foreach ($ingredientsAlphabetically as $ingredient)
+        {{-- Ingredient Name --}}
+        <p>{{$ingredient->name}} </p>    
+       
+        <button wire:click="ingredientInfo({{$ingredient->apiIngredientId}})">show info</button>
 
-                @endforeach
-            </div>
+    @endforeach
+</div>
