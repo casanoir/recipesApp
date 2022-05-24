0401040e4113c8d3c7da28c22d30c3647322fb93
casanoir <mendoun@hotmail.de>
2022-05-24 23:46:56 +0200

    create Ingr info component & get data

--- app/Http/Livewire/IngredientInfo.php
+++ app/Http/Livewire/IngredientInfo.php
@@ -3,17 +3,52 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 
 class IngredientInfo extends Component
 {
+    
     public $apiIngredientId;
-    protected $listeners =['ingredientInfo'=>'getIngredientInfo'];
+    public $ingredientData;
+    public $ingredientSubstitutes;
+    
+    protected $listeners =['ingredientInfo'=>'update'];
+    
     public function render()
     {
         return view('livewire.ingredient-info');
     }
-    public function getIngredientInfo(){
-        $this->apiIngredientId=2;
+
+    // Get all available information about an ingredient
+    public function getIngredientInfo($id){
+        $response = Http::acceptJson()->get('https://api.spoonacular.com/food/ingredients/'.$id.'/information?', [
+            'apiKey'=>env('CLIENT_API_KEY'),
+            'amount' => 1,
+        ]);
+        $this->ingredientData = $response->json();
+        return $this->ingredientData;
+    }
+
+    // Search for substitutes for a given ingredient.
+    public function getIngredientSubstitutes($id){
+        $response = Http::acceptJson()->get('https://api.spoonacular.com/food/ingredients/'.$id.'/substitutes', [
+            'apiKey'=>env('CLIENT_API_KEY'),
+        ]);
+        $this->ingredientSubstitutes = $response->json();
+        return $this->ingredientSubstitutes;
+    }
+
+    // Update Method nested with AllIngredients Blade ->Btn show ingeredient info
+    public function update($apiIngredientId){
+        
+        $this->apiIngredientId=$apiIngredientId;
+
+        $this->getIngredientInfo($apiIngredientId);
+        
+        $this->getIngredientSubstitutes($apiIngredientId);
         
     }
+
 }
+
--- resources/views/livewire/ingredient-info.blade.php
+++ resources/views/livewire/ingredient-info.blade.php
@@ -1,9 +1,13 @@
 <div>
     @isset($apiIngredientId)
-        <h1>apiIngredientId</h1>
+        <h1>{{$apiIngredientId}}</h1>
+        {{-- <h3>{{$ingredientData->name}}</h3> --}}
+        {{-- @foreach($apiIngredientId as $item)
+            {{$item->name}}
+        @endforeach --}}
     @endisset
 
     @empty($apiIngredientId)
-        <h2>2</h2>
+        <h2>RECIPES AT HOME</h2>
     @endempty
 </div>
