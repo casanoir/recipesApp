988eefc4460d66c38cccde0d4c308f09e868ca70
aliSyntra <ali.elbaz@cursist.syntra.be>
2022-05-25 16:01:26 +0200

    update userModal

---
+++ resources/views/livewire/profile-modal.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <div class="row mt-5">
+        <div class="col-md-6 offset-3">
+            <div class="card">
+                <div class="card-header bg-info text-white">
+                    <h5><strong>Laravel Livewire Bootstrap Modal Example - NiceSnippets.com</strong></h5>
+                </div>
+                <div class="card-body">
+                    <button type="button" class="btn btn-primary" >
+                        Open Modal
+                    </button>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
