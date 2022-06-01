c6778f0cf54e1591e42a81dff7c0bff8fa0f5ea7
casanoir <mendoun@hotmail.de>
2022-06-01 21:04:06 +0200

    add comments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,18 +22,29 @@
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <link href="{{ asset('css/navbarFooter.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!--Livewire Styles-->
+    @livewireStyles
 
     <!--Font Awesome Icons-->
     <script src="https://kit.fontawesome.com/d8d68bfe42.js" crossorigin="anonymous"></script>
+    
+    <!--Alpine Js-->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    @livewireStyles
+    <!--Bootstrap Script-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    
 </head>
 <body >
-    {{-- app content --}}
+
+    {{-- App content --}}
     @yield('content')
+
+
+ 
 
     {{-- livewire Script --}}
     @livewireScripts
