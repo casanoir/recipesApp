60dceac54a603640b3ecf56b2fc905c4e8a4fcd9
casanoir <mendoun@hotmail.de>
2022-06-17 02:13:10 +0200

    edit home add 3 btn

--- resources/views/livewire/ingredients/my-ingredient.blade.php
+++ resources/views/livewire/ingredients/my-ingredient.blade.php
@@ -1,9 +1,10 @@
 
-<div>
+<div class="mt-3">
+    <h3 class="text-center m-3">My Ingredient</h3>
     <div class="w-full flex pb-10">
         @if($myIngredients->isNotEmpty())
       <div class="w-3/6 mx-1">
-          <input wire:model.debounce.300ms="search" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"placeholder="Search users...">
+          <input wire:model.debounce.300ms="search" type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"placeholder="Search your ingredient...">
       </div>
       <div class="w-1/6 relative mx-1">
           <select wire:model="sortField" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
