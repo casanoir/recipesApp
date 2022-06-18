d9307442f8c9d55c226eafd614272a2bf8e11c68
casanoir <mendoun@hotmail.de>
2022-06-19 00:00:33 +0200

    Index controller if auth return view Home

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 class IndexController extends Controller
 {
 
     public function index()
     {
-        
+        if (!isset(Auth::user()->id)){
             return view('pages.index');
+        }
+        else{
+            return view('pages.dashboard.home');  
+        }
 
     }
 
