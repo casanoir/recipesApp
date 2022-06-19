49772f8a61823cc37836311ea6048ddd7473e163
casanoir <mendoun@hotmail.de>
2022-06-19 05:27:59 +0200

    my recipes change btn if missedIngredientCount=0

--- resources/views/livewire/recipes/my-recipes.blade.php
+++ resources/views/livewire/recipes/my-recipes.blade.php
@@ -7,9 +7,15 @@
                 <img src="https://spoonacular.com/recipeImages/{{$recipe['id']}}-312x231.jpg" alt="{{$recipe['title']}}" class="card__image">
                 
                 <h4 class="card__title">{{$recipe['title']}}</h4>
-                <p class="card__description">You need {{$recipe['missedIngredientCount']}} ingredients more to do that recipe.</p>
+            @if($recipe['missedIngredientCount'] == 0)
+            </div>
+            <a href="/recipe/{{$recipe['id']}}" type="button" class="card__btn">Use It</a>
+            @else
+                    <p class="card__description">You need {{$recipe['missedIngredientCount']}} ingredients more to do that recipe.</p>
             </div>
             <a href="/recipe/{{$recipe['id']}}" type="button" class="card__btn">View Recipe</a>
+            @endif
+            
         </div>
         @endforeach
     </div>
