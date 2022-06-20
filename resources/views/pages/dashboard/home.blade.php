8da1cd3368d4d59271a214543646540ab71fe4e5
EvilCookies <berre.707@hotmail.com>
2022-06-20 15:53:02 +0200

    unifying look on all pages + couple bugfixes

--- resources/views/pages/dashboard/home.blade.php
+++ resources/views/pages/dashboard/home.blade.php
@@ -3,17 +3,23 @@
 @section('content')
 {{-- navbar  --}}
 @include ('layouts.layout.navbar')
+
+<!--Sidebar toggle button-->
+<div class="row">
+    <button id="sidebarToggler" class="navbar-toggler btn btn-primary smallScreenOnly" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarSupportedContent" aria-controls="sidebarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle sidebar') }}">
+        <i class="fas fa-bars"></i>
+    </button>
+</div>
+
 <!-- home Page content-->
 <div class="row" style="width:100%; margin:0 ;">    
     {{-- BUTTON TO LINK TO HOME COMPONENTS --}}
-        <div class="col-md-3 p-5 " style="background-color:  rgb(0 80 64); height: 100vh; " >
+        <div id="sidebarSupportedContent" class="col-md-3 p-5 collapse show" style=" " >
             @livewire('home.home-btn')
         </div>
     {{-- HOME COMPONENTS --}}
-        <div class="col-md-9" style="background-color: rgb(242, 242, 242); height: 100vh;">
-
-            @livewire('home.home-components')
-   
+        <div id="mainSupportedContent" class="col-md-9">    
+            @livewire('home.home-components')  
         </div>
 </div>
 @endsection
--- resources/views/pages/dashboard/recipes/recipes.blade.php
+++ resources/views/pages/dashboard/recipes/recipes.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 @section('content')
-        {{-- navbar  --}}
-        @include ('layouts.layout.navbar')
-        <!-- home Page content-->
-        <div class="row" style="width:100%; margin:0 ;">    
+{{-- navbar  --}}
+@include ('layouts.layout.navbar')
+
+<!--Sidebar toggle button-->
+<div class="row">
+        <button id="sidebarToggler" class="navbar-toggler btn btn-primary smallScreenOnly" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarSupportedContent" aria-controls="sidebarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle sidebar') }}">
+                <i class="fas fa-bars"></i>
+        </button>
+</div>
+<!-- home Page content-->
+<div class="row" style="width:100%; margin:0 ;">    
         {{-- All Ingredients Alphabetically --}}
-                <div class="col-md-3 " style="background-color:  rgb(0 80 64); height: 100vh; " >
-                        @livewire('recipes.meal-types')
-                </div>
-        {{-- Show Ingredient details --}}
-                <div class="col-md-9" style="background-color: rgb(242, 242, 242); height: 100vh;">
-                        
-                        @livewire('recipes.recipes-by-meal-type') 
-                </div>
+        <div id="sidebarSupportedContent" class="col-md-3 p-5 collapse show">
+                @livewire('recipes.meal-types')
         </div>
+        {{-- Show Ingredient details --}}
+        <div id="mainSupportedContent" class="col-md-9">
+                @livewire('recipes.recipes-by-meal-type') 
+        </div>
+</div>
 @endsection
