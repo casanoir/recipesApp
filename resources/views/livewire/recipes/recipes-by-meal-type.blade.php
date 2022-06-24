1cf33170a26f6bbba3fd2d888f691d7d0f74a7c0
EvilCookies <berre.707@hotmail.com>
2022-06-24 13:01:53 +0200

    made css and html for favorite recipe BTN

--- resources/views/livewire/recipes/recipes-by-meal-type.blade.php
+++ resources/views/livewire/recipes/recipes-by-meal-type.blade.php
@@ -6,10 +6,10 @@
             @foreach ($recipesByMealType['recipes'] as $recipe)
             <div class="card">
                 <div class="card__body">
+                    @livewire('components.fav-recipes-btn',['recipeId'=>$recipe['id']])
                     <img src="https://spoonacular.com/recipeImages/{{$recipe['id']}}-312x231.jpg" alt="{{$recipe['title']}}" class="card__image">
                     
                     <h4 class="card__title">{{$recipe['title']}}</h4>
-                    @livewire('components.fav-recipes-btn',['recipeId'=>$recipe['id']])
                 </div>
                 <a href="/recipe/{{$recipe['id']}}" type="button" class="card__btn">View Recipe</a>
             </div>
